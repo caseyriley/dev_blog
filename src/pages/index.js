d724b5448f20967fbed8d34ea5f838238d5ede0d
Casey <innerforest7@gmail.com>
2021-03-12 22:08:53 -0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useSiteMetadata } from '../hooks/useSiteMetadata';
+
+const StyledH1 = styled.h1`
+  color: rebeccapurple;
+`;
+
+export default () => {
+  const { title, description } = useSiteMetadata();
+  return (
+    <>
+      <StyledH1>{title}</StyledH1>
+      <p>{description}</p>
+    </>
+  );
+};
