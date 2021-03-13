ca42d42fe65d21f4feaa8f954a1f08f435ee8de2
Casey <innerforest7@gmail.com>
2021-03-13 15:19:36 -0800

    add links to blog pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,29 @@
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import React from 'react';
-import Dump from '../components/Dump';
+import styled from 'styled-components';
 import { Layout } from '../components/Layout';
+
+const IndexWrapper = styled.main``;
+
+const PostWrapper = styled.div``;
 
 export default ({ data }) => {
   return (
-    <>
-      <Layout>
-        <Dump data={data} />
-        {data.allMdx.nodes.map(({ excerpt, frontmatter }) => (
-          <>
-            <h1>{frontmatter.title}</h1>
-            <p>{frontmatter.date}</p>
-            <p>{excerpt}</p>
-          </>
+
+    <Layout>
+      <IndexWrapper>
+        {data.allMdx.nodes.map(
+          ({ excerpt, frontmatter, fields }) => (
+          <PostWrapper>
+            <Link to={fields.slug}>
+              <h1>{frontmatter.title}</h1>
+              <p>{frontmatter.date}</p>
+              <p>{excerpt}</p>
+            </Link>
+          </PostWrapper>
         ))}
-      </Layout>
-    </>
+      </IndexWrapper>
+    </Layout>
   );
 };
 
@@ -32,6 +39,9 @@ export const query = graphql`
         frontmatter {
           title
           date
+        }
+        fields {
+          slug
         }
       }
     }
