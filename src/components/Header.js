8304520eaa2681027cb1c148ad7ca935c2585575
Casey Riley <innerforest7@gmail.com>
2021-05-20 14:02:02 -0700

    add HeaderStyles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-import {Link} from 'gatsby';
-import React from 'react';
+import { Link } from "gatsby";
+import React from "react";
+import styled from 'styled-components';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Farro:wght@500&family=Raleway&display=swap');
+</style>
+const HeaderStyles = styled.header`
+font-family: 'Raleway', 'Farro', sans-serif;
+margin: 20px 20px 20px 20px;
+`;
 
-export const Header = ({ siteTitle, siteDescription}) => (
-  <Link to="/">
-    <h1>{siteTitle}</h1>
-    <p>{siteDescription}</p>
-  </Link>
+export const Header = ({ siteTitle, siteDescription }) => (
+  <HeaderStyles>
+    <Link to="/">
+      <h1>{siteTitle}</h1>
+      <p>{siteDescription}</p>
+    </Link>
+  </HeaderStyles>
 );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,15 @@ import {Header} from './Header'
 
 const AppStyles = styled.main`
 
-  margin: 0 auto;
+  margin: 0;
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0px;
+  left; 0px;
+  right: 0px;
+  bottom: 0px;
+  overflow-y: scroll;
 `;
 
 export const Layout = ({children}) => {
