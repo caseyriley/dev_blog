ac3c582d0d52790d003161b1033875cc55f3bee0
caseyriley <innerforest7@gmail.com>
2021-05-30 17:30:08 -0700

    make localhost script

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { Link, graphql } from "gatsby";
 import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
 // import Img from 'gatsby-image';
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import { Layout } from "../components/Layout";
 
 import "./index.css";
@@ -11,13 +11,14 @@ import "./index.css";
   url('https://fonts.googleapis.com/css2?family=Farro:wght@500&family=Raleway&display=swap');
 </style>;
 
-const IndexWrapper = styled.main`
-  font-family: "Raleway", "Farro", sans-serif;
-  margin: 20px 0px 20px 0px;
-`;
+// const IndexWrapper = styled.main`
+//   font-family: "Raleway", "Farro", sans-serif;
+//   margin: 20px 0px 20px 0px;
+// `;
 
-const PostWrapper = styled.div`
-`;
+// const PostWrapper = styled.div`
+//   border: 2px solid green;
+// `;
 
 // const Image = styled(Img)`
 //   border-radius: 5px;
@@ -27,31 +28,31 @@ const indexComponent = ({ data }) => {
   console.log("data", data);
   return (
     <Layout>
-      <IndexWrapper>
+      < div className={"index-wrapper"}>
         {/* <Dump data={data}></Dump> */}
         {data.allMdx.nodes.map(({ id, excerpt, frontmatter, fields }) => (
           // console.log("data.allMdx=======>",data.allMdx)
           // const image = frontmatter.cover.publicURL;
-          <PostWrapper key={id}>
-            <Link class={"link"} to={fields.slug}>
-              {console.log(
-                "frontmatter.cover.childImageSharp.fixed=====>",
-                frontmatter.cover.publicURL
-              )}
 
-              <GatsbyImage
-                image={frontmatter.cover.childImageSharp.gatsbyImageData}
-                alt={"blog-image"}
-              />
-              {/* <img className={"post-img"} src={frontmatter.cover.publicURL} /> */}
+          <Link key={id} class={"link post-wrapper"} to={fields.slug}>
+            {console.log(
+              "frontmatter.cover.childImageSharp.fixed=====>",
+              frontmatter.cover.publicURL
+            )}
 
-              <h1 className="blog-title">{frontmatter.title}</h1>
-              <p>{frontmatter.date}</p>
-              <p>{excerpt}</p>
-            </Link>
-          </PostWrapper>
+            <GatsbyImage
+              className={"post-image"}
+              image={frontmatter.cover.childImageSharp.gatsbyImageData}
+              alt={"blog-image"}
+            />
+            {/* <img className={"post-img"} src={frontmatter.cover.publicURL} /> */}
+
+            <h1 className="blog-title">{frontmatter.title}</h1>
+            <p className={"blog-p"}>{frontmatter.date}</p>
+            <p className={"blog-p"}>{excerpt}</p>
+          </Link>
         ))}
-      </IndexWrapper>
+      </div>
     </Layout>
   );
 };
