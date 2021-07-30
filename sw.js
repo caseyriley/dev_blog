c520e349f9f4cb9329d99d7d47bdaeaa0f901579
caseyriley <innerforest7@gmail.com>
2021-07-30 00:05:04 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1abd2e21fe65a27c29f1.js"
+    "url": "webpack-runtime-f9f026be73662c58987f.js"
   },
   {
     "url": "framework-ba71c5098c1c2b465271.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3ab31d6045dccfbee3aa1d076aafaf61"
+    "revision": "cef6353bf9f100ca55c639f0ec019016"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-385885db016d2846214a.js"
@@ -51,7 +51,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "051c0dfd84a39cdb0289b7b86dd59195"
+    "revision": "7948df9256c14a6a512ee58cb139b297"
   },
   {
     "url": "polyfill-9e7bc54870b608d48a3e.js"
