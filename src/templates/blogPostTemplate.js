da535d111fa3d590bdab6670cc4c20b5f5fbb07c
caseyriley <innerforest7@gmail.com>
2021-08-02 22:54:25 -0700

    add likeButton.css

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -5,7 +5,8 @@ import React from "react";
 import { Layout } from "../components/Layout";
 import styled from "styled-components";
 import "./index.css";
-import "./gradientSelector.css"
+import "./gradientSelector.css";
+import "./likeButton.css";
 
 const Background = styled.main`
   width: 100%;
